25845869509a3419c6147aba0d2bb7b6d2d297cf
sursindmitry <dmitrijsursin@gmail.com>
2021-10-03 18:28:02 +0700

    Add JFrame

---
+++ src/com/company/Main.java
@@ -0,0 +1,17 @@
+package com.company;
+
+
+import javax.swing.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame();
+        frame.setTitle("Калькулятор");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setBounds(600,300,640,480);
+        CalcPanel panel = new CalcPanel();
+        frame.add(panel);
+        frame.setVisible(true);
+    }
+}
