d759835ebb6996442fe32d70ee479ffb4bd6a574
sursindmitry <dmitrijsursin@gmail.com>
2021-10-04 23:19:12 +0700

    + - * /

--- src/com/company/CalcPanel.java
+++ src/com/company/CalcPanel.java
@@ -2,47 +2,115 @@ package com.company;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Objects;
 
 public class CalcPanel extends JPanel {
-    JPanel panel = new JPanel();
+    private JPanel panel = new JPanel();
+    private boolean start;
+    private String lastCommand;
+    private JButton display;
+    private double result;
 
     public CalcPanel(){
         super();
+        start = true;
+        lastCommand = "=";
+        result = 0;
+
         setLayout(new BorderLayout());
-        JButton dispaly = new JButton("0");
-        dispaly.setEnabled(false);
-        add(dispaly, BorderLayout.NORTH);
+        display = new JButton("0");
+        display.setEnabled(false);
+        add(display, BorderLayout.NORTH);
 
         panel = new JPanel();
         panel.setLayout(new GridLayout(4,4));
-        addButton("7");
-        addButton("8");
-        addButton("9");
-        addButton("/");
 
-        addButton("4");
-        addButton("5");
-        addButton("6");
-        addButton("*");
+        InsertAction insert = new InsertAction();
+        ComandAction command = new ComandAction();
 
-        addButton("1");
-        addButton("2");
-        addButton("3");
-        addButton("-");
+        addButton("7",insert);
+        addButton("8",insert);
+        addButton("9",insert);
+        addButton("/",command);
 
-        addButton("0");
-        addButton(".");
-        addButton("=");
-        addButton("+");
+        addButton("4",insert);
+        addButton("5",insert);
+        addButton("6",insert);
+        addButton("*",command);
+
+        addButton("1",insert);
+        addButton("2",insert);
+        addButton("3",insert);
+        addButton("-",command);
+
+        addButton("0",insert);
+        addButton(".",insert);
+        addButton("=",command);
+        addButton("+",command);
 
 
         add(panel,BorderLayout.CENTER);
 
     }
 
-    private  void addButton(String lable){
+    private  void addButton(String lable, ActionListener listener){
         JButton button = new JButton(lable);
+        button.addActionListener(listener);
         panel.add(button);
+    }
+    
+    private class InsertAction implements ActionListener{
+
+        @Override
+        public void actionPerformed(ActionEvent ae){
+            String input = ae.getActionCommand();
+            if(start){
+                display.setText("");
+                start=false;
+            }
+            if (!Objects.equals(input, ".") || !display.getText().contains("."))
+                display.setText(display.getText() + input);
+
+        }
+    }
+
+    private class ComandAction implements ActionListener{
+        @Override
+        public void actionPerformed(ActionEvent ae){
+
+            String command = ae.getActionCommand();
+            if (start){
+                lastCommand = command;
+            }
+            else{
+                calc(Double.parseDouble(display.getText()));
+                lastCommand = command;
+                start=true;
+            }
+        }
+
+        public void calc(double x){
+            switch (lastCommand) {
+                case "+":
+                    result += x;
+                    break;
+                case "-":
+                    result -= x;
+                    break;
+                case "*":
+                    result *= x;
+                    break;
+                case "/":
+                    result /= x;
+                    break;
+                case "=":
+                    result = x;
+                    break;
+            }
+            display.setText("" + result);
+        }
     }
 
 }
